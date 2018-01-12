35208e6bb7ac8deebbe3124003f101429bdd6f74
hexinze <hexinze@ajmide.com>
2018-01-12 10:56:06 +0800

    add all

---
+++ run.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify
+from random import *
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__,
+            static_folder = "./dist/static",
+            template_folder = "./dist")
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if app.debug:
+        return requests.get('http://localhost:8080/{}'.format(path)).text
+    return render_template("index.html")
+
+
+
+if __name__ == '__main__':
+    # socketio.run(app, debug=True, port=5001)
+    # socketio.run(app, debug=True, host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
